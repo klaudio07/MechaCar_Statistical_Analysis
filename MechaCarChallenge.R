ced48fdd26d1aba1d8efa5d13b8f399dc081b6ed
klaudio07 <67558570+klaudio07@users.noreply.github.com>
2020-10-04 17:38:49 -0400

    Deliverable 1: Linear Regression to Predict MPG

---
+++ MechaCarChallenge.R
@@ -0,0 +1,8 @@
+#load the dplyr package
+library(dplyr)
+#Import and read in the MechaCar_mpg.csv
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+#create multiple linear model
+lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar_mpg)
+#generate summary stats
+summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar_mpg))
