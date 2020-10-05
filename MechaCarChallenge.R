d95909e274e2cdf7a388bbce056a3cd1282ce583
klaudio07 <67558570+klaudio07@users.noreply.github.com>
2020-10-04 20:35:38 -0400

    Update Challenge code with Deliverables 1 to 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -22,3 +22,27 @@ summarize_demo <- lot_summary %>% group_by(Manufacturing_Lot) %>% summarize()
 #  RScript that creates a lot_summary dataframe to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column.
 lot_summary <- Suspension_Coil_data %>% group_by(Manufacturing_Lot) %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
+## Deliverable 3 T-Tests on Suspension Coils
+
+# Test for Normality 
+shapiro.test(Suspension_Coil_data$PSI)
+# PSi distribution is normal since P-value is way less than 0.05
+
+# RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(x=Suspension_Coil_data$PSI, mu=1500)
+
+# Write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+subset_Lot1 <- subset(Suspension_Coil_data, Suspension_Coil_data$Manufacturing_Lot == 'Lot1')
+t.test(x=subset_Lot1$PSI, mu=1500)
+
+subset_Lot2 <- subset(Suspension_Coil_data, Suspension_Coil_data$Manufacturing_Lot == 'Lot2')
+t.test(x=subset_Lot2$PSI, mu=1500)
+
+subset_Lot3 <- subset(Suspension_Coil_data, Suspension_Coil_data$Manufacturing_Lot == 'Lot3')
+t.test(x=subset_Lot3$PSI, mu=1500)
+
+
+
+
+
+
